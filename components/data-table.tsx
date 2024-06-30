87dfadf4666c234cbc522a7f2c7fdd43a48665d0
fabriciomg29 <fabricio.goulart.dev@gmail.com>
2024-06-30 12:19:39 -0300

    maked component to confirm actions

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -24,6 +24,7 @@ import {
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Trash } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -40,6 +41,10 @@ export function DataTable<TData, TValue>({
     onDelete,
     disabled,
 }: DataTableProps<TData, TValue>) {
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You are about to perform a bulk delete"
+    );
     const [sorting, setSorting] = React.useState<SortingState>([]);
     const [columnFilters, setColumnFilters] =
         React.useState<ColumnFiltersState>([]);
@@ -65,6 +70,7 @@ export function DataTable<TData, TValue>({
 
     return (
         <div>
+            <ConfirmDialog />
             <div className="flex items-center py-4">
                 <Input
                     placeholder={`Filter ${filterKey}`}
@@ -85,10 +91,14 @@ export function DataTable<TData, TValue>({
                     size="sm"
                     variant="outline"
                     className="ml-auto font-normal text-xs"
-                    disabled={disabled}
-                    onClick={() => {
-                        onDelete(table.getFilteredSelectedRowModel().rows);
-                        table.resetRowSelection();
+                    disabled={disabled || !table.getFilteredSelectedRowModel().rows.length}
+                    onClick={async () => {
+                        const ok = await confirm()
+
+                        if(ok) {
+                            onDelete(table.getFilteredSelectedRowModel().rows);
+                            table.resetRowSelection();
+                        }
                     }}
                 >
                     <Trash className="size-4 mr-2" />
---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+
+type useConfirmResponseProps = [() => JSX.Element, () => Promise<unknown>];
+
+export const useConfirm = (
+    title: string,
+    message: string
+): useConfirmResponseProps => {
+    const [promise, setPromise] = useState<{
+        resolve: (value: boolean) => void;
+    } | null>(null);
+
+    const confirm = () =>
+        new Promise((resolve, reject) => {
+            setPromise({ resolve });
+        });
+
+    const handleClose = () => {
+        setPromise(null);
+    };
+
+    const handleConfirm = () => {
+        promise?.resolve(true);
+        handleClose();
+    };
+
+    const handleCancel = () => {
+        promise?.resolve(false);
+        handleClose();
+    };
+
+    const ConfirmationDialog = () => (
+        <Dialog open={!!promise}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{message}</DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="pt-2">
+                    <Button onClick={handleCancel} variant="outline">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleConfirm}>Confirm</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+
+    return [ConfirmationDialog, confirm];
+};
